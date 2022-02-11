0cb5c5acbf5766dfe730215dd5387e472816ee52
Natasha Fernandez <natasha.fernandez.arg@gmail.com>
2022-02-11 18:55:19 -0300

    Add ContactDetail, ContactEdit

---
+++ src/components/contact/ContactDetail.jsx
@@ -0,0 +1,27 @@
+import Body from '../common/Body'
+import EditContact from './EditContact';
+import React, { useEffect } from 'react';
+
+const ContactDetail = ({contact, titleSetter, contacActions}) => {
+
+    useEffect(() => {
+      titleSetter("Contacto");
+    });
+
+    return(
+        <Body className='contact-detail' 
+            onActionComponent={
+                <EditContact contact={contact} contacActions={contacActions} titleSetter={titleSetter}/>
+            }>
+            <section className='contact-detail__media'>
+                <span className='contact-detail__media__avatar--empty'>{`${contact.name[0].toUpperCase()}${contact.lastName[0].toUpperCase()}`}</span>
+                <h2 className='contact-detail__media__fullname'>{`${contact.name.toUpperCase()} ${contact.lastName.toUpperCase()}`}</h2>
+            </section>
+            <section className='contact_info'>
+                <p>{contact.phoneNumber}</p>
+            </section>
+        </Body>
+    );
+}
+
+export default ContactDetail;
---
+++ src/components/contact/EditContact.jsx
@@ -0,0 +1,41 @@
+import ContactForm from './ContactForm';
+import { useEffect, useState } from 'react';
+
+const EditContact = ({contact, contactActions, titleSetter}) => {
+
+    useEffect(() => {
+        titleSetter("Editar Contacto");
+    })
+
+    const [name, setName] = useState(contact.name)
+    const [lastName, setLastName] = useState(contact.lastName)
+    const [phoneNumber, setPhoneNumber] = useState(contact.phoneNumber)
+
+    /**atributos del contacto a usar en el formulario*/
+    const contactAttributes = {
+        name: {getValue:() => name, setValue:(newValue) => setName(newValue), label: "Nombre"},
+        lastName: {getValue:() => lastName, setValue:(newValue) => setLastName(newValue), label: "Apellido"}, 
+        phoneNumber: {getValue:() => phoneNumber, setValue:(newValue) => setPhoneNumber(newValue), label: "Telefono"}
+    }
+
+    /**Obtiene las entries del objeto {@linkcode contactAttributes} */
+    const getContactAttributesCollection = () => Object.entries(contactAttributes);
+
+    /**genera un contacto con los nombres de attributos para un contacto y sus valores y lo guarda en la lista*/
+    const saveContact = () => {
+
+        //reducir los atributos de la coleccion a un objeto contacto de clave-valor para ser añadido a la lista
+        const newContact = getContactAttributesCollection().reduce((contact,[attributeName,attribute]) => {
+            contact[attributeName] = attribute.getValue();
+            return contact;
+        }, {});
+
+        contactActions({type: "editContact", payload: newContact});
+    }
+
+    return(
+        <ContactForm title="Editar Contacto" titleSetter={titleSetter} contactAttributes={getContactAttributesCollection()} onSubmitContact={saveContact}/>
+    );
+}
+
+export default EditContact;
