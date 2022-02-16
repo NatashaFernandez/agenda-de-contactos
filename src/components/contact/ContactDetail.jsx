fff2ca459f77f6e0ce70ea16a57c7596bca5e2f6
Natasha Fernandez <nfernandez@msa.com.ar>
2022-02-16 16:39:51 -0300

    CAP-9 Add buttons message and phone

--- src/components/contact/ContactDetail.jsx
+++ src/components/contact/ContactDetail.jsx
@@ -1,8 +1,11 @@
 import Body from "../common/Body";
 import EditContact from "./EditContact";
 import React, { useEffect } from "react";
+import goBackIcon from "../../img/go-back.svg";
+import messageIcon from "../../img/envelope-solid.svg"
+import phoneIcon from "../../img/phone-solid.svg"
 
-const ContactDetail = ({ contact, titleSetter, contacActions }) => {
+const ContactDetail = ({contact, titleSetter, contacActions, onGoback}) => {
   useEffect(() => {
     titleSetter("Contacto");
   });
@@ -11,11 +14,16 @@ const ContactDetail = ({ contact, titleSetter, contacActions }) => {
     <Body
       className="contact-detail"
       onActionComponent={
-        <EditContact
-          contact={contact}
-          contacActions={contacActions}
-          titleSetter={titleSetter}
-        />
+        <>
+          <button className="body__go-back-btn" onClick={onGoback}>
+            <img src={goBackIcon} alt="botón para volver atras" />
+          </button>
+          <EditContact
+            contact={contact}
+            contacActions={contacActions}
+            titleSetter={titleSetter}
+          />
+        </>
       }
     >
       <section className="contact-detail__media">
@@ -23,7 +31,17 @@ const ContactDetail = ({ contact, titleSetter, contacActions }) => {
         <h2 className="contact-detail__media__fullname">{`${contact.name.toUpperCase()} ${contact.lastName.toUpperCase()}`}</h2>
       </section>
       <section className="contact_info">
-        <p>{contact.phoneNumber}</p>
+        <div className="contact_info--phone">
+          <a href={`tel:${contact.phoneNumber}`}>
+            <img src={phoneIcon} alt="Icono de telefono" />
+          </a>
+          <p>{contact.phoneNumber}</p>
+          <a href={`sms:${contact.phoneNumber}`}>
+            <img src={messageIcon} alt="icono de mensaje" />
+          </a>
+          
+          </div>
+        
       </section>
     </Body>
   );
